e43bbfe938b42df68fd775c47fd1a1f12b9f7095
jairomateus2011 <jairomateus2011@gmail.com>
2024-08-22 14:47:51 +0000

    Cuarto Commit bd y crear usuario 220824 v0

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -4,7 +4,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloword),
-    path('signup/', views.helloword),
-    
+    path('', views.home, name = 'home'),
+    path('signup/', views.signup, name = 'signup'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+"""
 def helloword (request):
     #return HttpResponse ('Hello Word!!')
     #title = 'Django'
@@ -10,3 +12,30 @@ def helloword (request):
         #'mytitle': title
         'form': UserCreationForm
     })
+"""
+def home(request):
+    return render (request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        print ('enviando formulario de datos (GET)')
+        return render (request, 'signup.html',{
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # registrar usuario
+                user = User.objects.create_user(
+                username = request.POST['username'],
+                password = request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado exitosamente')
+            except:
+                return HttpResponse('Usuario ya existe')
+           
+
+        return HttpResponse('Password no coincide')    
+
+        
+        

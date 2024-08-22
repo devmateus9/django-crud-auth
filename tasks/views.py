41cbda406e8b6cff972822375b95fb7130407180
jairomateus2011 <jairomateus2011@gmail.com>
2024-08-22 20:51:19 +0000

    decimo commit login

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
@@ -51,3 +51,26 @@ def signup(request):
 def tasks(request):
     return render (request, 'tasks.html')        
         
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render (request, 'signin.html',{
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username = request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render (request, 'signin.html',{
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+
+        
+    

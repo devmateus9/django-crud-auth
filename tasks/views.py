8a0a6e31530f890910deb1d67d2d5aa8edfefae9
jairomateus2011 <jairomateus2011@gmail.com>
2024-08-22 03:07:01 +0000

    Tercer Commit 210824 v1

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def helloword (request):
-    return HttpResponse ('Hello Word!!')
-    
+    #return HttpResponse ('Hello Word!!')
+    #title = 'Django'
+    return render (request, 'signup.html',{
+        #'mytitle': title
+        'form': UserCreationForm
+    })

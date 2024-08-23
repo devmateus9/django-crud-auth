e1393f1a489698d595bcd730374fb906aefd0788
jairomateus2011 <jairomateus2011@gmail.com>
2024-08-23 01:38:36 +0000

    formulario de tareas commit 220824

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
+
 
 # Create your views here.
 """
@@ -50,7 +52,19 @@ def signup(request):
 
 def tasks(request):
     return render (request, 'tasks.html')        
-        
+
+def create_task(request):
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:153928661.
+    if request.method == 'GET':
+        return render (request, 'create_task.html',{
+            'form': TaskForm
+        })
+    else:
+        print(request.POST)
+        return render (request, 'create_task.html',{
+            'form': TaskForm
+        })
+
 def signout(request):
     logout(request)
     return redirect('home')
